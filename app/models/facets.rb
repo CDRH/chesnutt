0db10c2671c3bd39c27470bba7df405e572fac07
Karin Dalziel <kdalziel2@unl.edu>
2020-06-23 10:16:11 -0500

    initial commit

---
+++ app/models/facets.rb
@@ -0,0 +1,46 @@
+module Facets
+
+  @facet_info = {
+    "category" => {
+      "label" => "Category",
+      "display" => true
+    },
+    "subcategory" => {
+      "label" => "Subcategory",
+      "display" => true
+    },
+    "date.year" => {
+      "label" => "Year",
+      "display" => true
+    },
+    "creator.name" => {
+      "label" => "Creator",
+      "display" => true,
+    },
+    "format" => {
+      "label" => "Format",
+      "display" => true
+    },
+    "places" => {
+      "label" => "Places",
+      "display" => true
+    },
+    "source" => {
+      "label" => "Source",
+      "display" => true
+    }
+  }
+
+  # Remove project key if API URL uses a specific collection
+  if /\/collection\/\w+$/.match(API_PATH)
+    @facet_info.delete("project")
+  end
+
+  def self.facet_info
+    return @facet_info
+  end
+
+  def self.facet_list
+    return @facet_info.keys
+  end
+end
