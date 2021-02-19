678bc9232f0d66d9dae2238a628d254d539ad5a2
Karin Dalziel <kdalziel2@unl.edu>
2021-02-19 12:16:27 -0600

    changed colors, made test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "design", to: "general#temp_design", as: "design"
+
+
 end
