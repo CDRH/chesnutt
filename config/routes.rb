8cad9b0c10b664e466861e8688cc69e12661fc95
Karin Dalziel <kdalziel2@unl.edu>
2021-02-22 14:08:23 -0600

    trying to work out subnav

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,13 @@ Rails.application.routes.draw do
   get "scholarship/bibliography", to: "items#scholarship_bibliography", as: "scholarship_bibliography"
 
   get "about", to: "general#about", as: "about"
+  get "about/history", to: "general#about_history", as: "about_history"
+  get "about/editorial_policy", to: "general#about_editorial_policy", as: "about_editorial_policy"
+  get "about/encoding_guidelines", to: "general#about_encoding_guidelines", as: "about_encoding_guidelines"
+  get "about/use_statement", to: "general#about_use_statement", as: "about_use_statement"
+  get "about/people", to: "general#about_people", as: "about_people"
+  get "about/biographical_sketch", to: "general#about_biographical_sketch", as: "about_biographical_sketch"
+  get "about/white_paper", to: "general#about_white_paper", as: "about_white_paper"
 
 
   # temp page to work out some design options
