8466701f7dd1b9df853b588d958e7434b7c6820d
Karin Dalziel <kdalziel2@unl.edu>
2021-02-23 10:36:37 -0600

    adding more pages, design tweaks

--- app/controllers/items_override.rb
+++ app/controllers/items_override.rb
@@ -1,13 +1,76 @@
 ItemsController.class_eval do
 
   def works
-    # query to return only souvenir documents
       options = params.permit!.deep_dup
       options["f"] = ["category|Works"]
       @res = $api.query(options)
 
       # render search preset with route information
       @route_path = "works_path"
+      render_overridable "items", "works"
+
+      @title = "All Works"
+  end
+
+  def works_stories
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Stories"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_stories_path"
+      render_overridable "items", "works"
+
+      @title = "Stories"
+  end
+
+  def works_essays
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Essays"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_essays_path"
+      render_overridable "items", "works"
+  end
+
+  def works_novels
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Novel"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_novels_path"
+      render_overridable "items", "works"
+  end
+
+  def works_poems
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Poems"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_poems_path"
+      render_overridable "items", "works"
+  end
+
+  def works_collection
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Collection"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_collection_path"
+      render_overridable "items", "works"
+  end
+  
+  def works_other
+      options = params.permit!.deep_dup
+      options["f"] = ["subcategory|Other Works"]
+      @res = $api.query(options)
+
+      # render search preset with route information
+      @route_path = "works_other_path"
       render_overridable "items", "works"
   end
 
